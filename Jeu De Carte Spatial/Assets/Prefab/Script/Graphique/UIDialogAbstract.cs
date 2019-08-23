cba26519d5b602d4e7ae5d974ff8234c1f809c2e
jdeon <jd.eymann@gmail.com>
2019-08-24 01:41:46 +0200

    EVO - Creation de boite de dialog
    Creation d'une boite de dialog simple et d'un autre avec confirmation

---
+++ Jeu De Carte Spatial/Assets/Prefab/Script/Graphique/UIDialogAbstract.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class  UIDialogAbstract {
+
+	protected GameObject goDialog;
+
+	protected string descriptionAction;
+
+	protected float height;
+	protected float width;
+
+	public UIDialogAbstract(string descriptionAction){
+		this.descriptionAction = descriptionAction;
+
+		GameObject goCanvas = UIUtils.getCanvas ();
+		goDialog = new GameObject ("ConfirmDialog");
+		goDialog.AddComponent<CanvasRenderer> ();
+		goDialog.transform.SetParent (goCanvas.transform);
+		goDialog.transform.localPosition = Vector3.zero;
+
+		Image i = goDialog.AddComponent<Image> ();
+		i.sprite = ConstanteInGame.spriteBackgroundCarte;
+
+		Vector2 vUICardSize = UIUtils.getUICardSize ();
+		height = vUICardSize.x / 3f;
+		width = vUICardSize.y * .75f;
+		goDialog.GetComponent<RectTransform> ().sizeDelta = new Vector2 (width, height);
+		Text txtDialog = UIUtils.createText ("TextDialog", goDialog, 3, 0, .2f, .9f * width, height*.6f);
+		txtDialog.text = descriptionAction;
+
+		goDialog.SetActive (false);
+
+	}
+
+	public void showDialog(){
+		goDialog.SetActive (true);
+	}
+
+	public void hideDialog(){
+		GameObject.Destroy(goDialog);
+	}
+}
--- Jeu De Carte Spatial/Assets/Prefab/Script/Graphique/UIDialogInfo.cs
+++ Jeu De Carte Spatial/Assets/Prefab/Script/Graphique/UIDialogInfo.cs
@@ -3,18 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIConfirmDialog : UIDialogAbstract {
-
-	private Button btnValid;
+public class UIDialogInfo : UIDialogAbstract {
 
 	private Button btnCancel;
 
-	public UIConfirmDialog(string descriptionAction) : base (descriptionAction) {
-
-		GameObject goBoutonValid = UIUtils.createPanel("BoutonValid",goDialog,width*(-0.25f),height*(-.25f), width*.25f,height*.25f);
-		Text txtValid = UIUtils.createText ("TextValidDialog", goBoutonValid, 1, 0, 0, .9f * width * .25f, height * .25f);
-		txtValid.text = "Valider";
-		btnValid = goBoutonValid.AddComponent<Button> ();
+	public UIDialogInfo(string descriptionAction) : base (descriptionAction) {
 
 		GameObject goBoutonCancel = UIUtils.createPanel("BoutonCancel",goDialog,width*(0.25f),height*(-.25f), width*.25f,height*.25f);
 		Text txtCancel = UIUtils.createText ("TextValidCancel", goBoutonCancel, 1, 0, 0, .9f * width * .25f, height * .25f);
@@ -32,10 +25,6 @@ public class UIConfirmDialog : UIDialogAbstract {
 
 	public void hideDialog(){
 		GameObject.Destroy(goDialog);
-	}
-
-	public Button BtnValid {
-		get{ return btnValid; }
 	}
 
 	public Button BtnCancel{
